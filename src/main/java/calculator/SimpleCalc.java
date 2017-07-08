17cff86aa1e202ba3956ab0cbc83c49a0c51fe1d
Demianenko <demyanenko.s.k@gmail.com>
2017-07-08 12:23:37 +0200

    1

--- src/main/java/calculator/SimpleCalc.java
+++ src/main/java/calculator/SimpleCalc.java
@@ -1,8 +1,5 @@
 package calculator;
 
-/**
- * Created by user on 1/9/2017.
- */
 public class SimpleCalc {
     private double value;
     public void plus(int a, int b) {
--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -1,31 +1,26 @@
 package steps;
 
 import calculator.SimpleCalc;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 import static org.testng.Assert.assertEquals;
-
-/**
- * Created by user on 1/9/2017.
- */
-public class Steps extends AbstractTestNGCucumberTests {
+public class Steps {
     private SimpleCalc calc;
 
     @Given("^a calculator I just turned on$")
     public void a_calculator_I_just_turned_on() {
         calc = new SimpleCalc();
     }
-
     @When("^I add (\\d+) and (\\d+)$")
     public void adding(int arg1, int arg2) {
         calc.plus(arg1,arg2);
     }
-
     @Then("^the result is (\\d+)$")
     public void the_result_is(double expected) {
         assertEquals(expected, calc.getValue());
     }
+
 }
